bfdd0ab59071e19d044cc2e957e0174e8bc6f4e3
dmedvedev01 <Dmitriy.A.Medvedev@bia-tech.ru>
2023-08-10 02:36:55 +0300

    Сделал скачивание oneget

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,39 @@
+import * as exec from '@actions/exec'
 import * as core from '@actions/core'
 import {wait} from './wait'
 
 async function run(): Promise<void> {
+  const platformType = process.platform
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    let extension = ''
+    let platform = ''
 
-    core.setOutput('time', new Date().toTimeString())
+    switch (platformType) {
+      case 'win32': {
+        extension = 'zip'
+        platform = 'Windows'
+        break
+      }
+      case 'darwin': {
+        extension = 'tar.gz'
+        platform = 'Darwin'
+        break
+      }
+      case 'linux': {
+        extension = 'tar.gz'
+        platform = 'Linux'
+        break
+      }
+      default: {
+        throw new Error('Not supported on this OS type')
+      }
+    }
+
+    await exec.exec(
+      `curl -L https://github.com/v8platform/oneget/releases/download/v0.6.0/oneget_${platform}_x86_64.${extension} --output oneget.${extension}`
+    )
+
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
