e7ad92a33e190895ec572472673091f252ed11ce
IT-Medved <it.medve.dev@yandex.com>
2023-08-13 01:52:28 +0300

    edt

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function installOneGet(version: string, platform: string): Promise<void> {
   const cacheKey = await cache.restoreCache([gstsrc], key)
 
   if (!cacheKey) {
-    core.info(`oneget cache not found; creating a new one. (key: "${key}")`)
+     core.info(`oneget cache not found; creating a new one. (key: "${key}")`)
 
     let extension
     if (platform === 'Windows') {
@@ -89,7 +89,7 @@ async function installEDT(version: string, platform: string): Promise<void> {
     //   core.info('wierd size of edt installers')
     // }
 
-    await exec(files[0], ['--ignore-hardware-checks', 'install'])
+    await exec(files[0], ['install', '--ignore-hardware-checks'])
 
     await cache.saveCache([gstsrc], key)
 
